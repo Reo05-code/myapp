cbe247b7eb0a12496ae89bb6250b064dfa0531b1
Reo05-code <t.asaishi05@gmail.com>
2025-10-23 14:38:45 +0900

    Rubocopの導入が完了

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,24 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe "静的ページ", type: :request do
-  describe "トップページ" do
-    it "正常なレスポンスを返すこと" do
+require 'rails_helper'
+
+RSpec.describe '静的ページ', type: :request do
+  describe 'トップページ' do
+    it '正常なレスポンスを返すこと' do
       get root_path
       expect(response).to have_http_status 200
     end
   end
 
-  describe "クックログとは？ページ" do
-    it "正常なレスポンスを返すこと" do
+  describe 'クックログとは？ページ' do
+    it '正常なレスポンスを返すこと' do
       get about_path
       expect(response).to have_http_status 200
     end
   end
 
-  describe "利用規約ページ" do
-    it "正常なレスポンスを返すこと" do
+  describe '利用規約ページ' do
+    it '正常なレスポンスを返すこと' do
       get use_of_terms_path
       expect(response).to have_http_status 200
     end
--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,1 +1,6 @@
-include ApplicationHelper 
+# frozen_string_literal: true
+
+# RSpec 用に ApplicationHelper をテストに読み込む
+RSpec.configure do |config|
+  config.include ApplicationHelper
+end
